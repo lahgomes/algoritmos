a0978d590dbe27cccdd1569db8f95e00365ebf4d
Larissa <larissa_gomes19@hotmail.com>
2021-11-09 07:20:37 -0300

    year11

---
+++ js/humanResourceMachine/year11.js
@@ -0,0 +1,9 @@
+function year11(number1, number2) {
+  const sub1 = number1 - number2
+  const sub2 = number2 - number1
+
+  console.log('Resultado da sub1: ', sub1)
+  console.log('Resultado da sub2: ', sub2)
+}
+
+year11(1, 5)
