1d9e5f2ddf288f8e08d71a66d67c716ba17a7b7a
Larissa <larissa_gomes19@hotmail.com>
2021-11-12 07:45:59 -0300

    year14

---
+++ js/humanResourceMachine/year14.js
@@ -0,0 +1,11 @@
+function year14(number1, number2) {
+  if (number1 > number2) {
+    console.log(number1)
+  } else if (number2 > number1) {
+    console.log(number2)
+  } else console.log(number1)
+}
+
+year14(10, 1)
+year14(1, 7)
+year14(1, 1)
