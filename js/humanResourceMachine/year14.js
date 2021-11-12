f9586cecc55b514e296b972f2e5cc4cb23a7edb0
Larissa <larissa_gomes19@hotmail.com>
2021-11-12 08:12:46 -0300

    year14 better

--- js/humanResourceMachine/year14.js
+++ js/humanResourceMachine/year14.js
@@ -1,10 +1,15 @@
-function year14(number1, number2) {
-  if (number1 > number2) {
-    console.log(number1)
-  } else if (number2 > number1) {
-    console.log(number2)
-  } else console.log(number1)
+if (number2 > number1) {
+  console.log(number2)
+} else {
+  console.log(number1)
 }
+// function year14(number1, number2) {
+//   if (number1 > number2) {
+//     console.log(number1)
+//   } else if (number2 > number1) {
+//     console.log(number2)
+//   } else console.log(number1)
+// }
 
 year14(10, 1)
 year14(1, 7)
