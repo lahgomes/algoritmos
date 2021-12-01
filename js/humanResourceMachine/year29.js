8154b9f4cd90630c95d1491effdaebbe68f16c06
Larissa <larissa_gomes19@hotmail.com>
2021-12-01 08:56:23 -0300

    year30

--- js/humanResourceMachine/year29.js
+++ js/humanResourceMachine/year29.js
@@ -7,5 +7,3 @@ const year29 = (numeros) => {
 console.log(year29(5))
 console.log(year29(9))
 console.log(year29(0))
-
-
---
+++ js/humanResourceMachine/year30.js
@@ -0,0 +1,21 @@
+const year30 = (numero) => {
+  const piso = ['g', 'e', 't', 0, 't', 'h', 0]
+  const pisoAndar = piso.slice(numero, piso.length)
+
+  const newArray = []
+  let achou = false
+  let counter = 0
+
+  while(!achou) {
+    if(pisoAndar[counter] === 0) { 
+      achou = true
+    } else { 
+      newArray.push(pisoAndar[counter])
+      counter++
+    }
+  }
+
+  return newArray
+}
+
+console.log(year30(2))
