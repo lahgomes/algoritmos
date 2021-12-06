3d3b756a118400b2bd8182a91df86367674a2f8b
Larissa Gomes <lahgomes@LAPTOP-PUQ31PEG.localdomain>
2021-12-06 07:44:58 -0300

    year34

---
+++ js/humanResourceMachine/year34.js
@@ -0,0 +1,13 @@
+const year34 = (letras) => {
+  const vogais = ['a', 'e', 'i','o', 'u']
+
+  if(!vogais.includes(letras)) {
+    console.log(letras)
+  }
+}
+
+year34(5)
+year34('a')
+year34('n')
+year34('u')
+
