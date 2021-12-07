4aa7978db311d0fd6c2acb7ec7d98e5719cb62ed
Larissa Gomes <lahgomes@LAPTOP-PUQ31PEG.localdomain>
2021-12-07 07:52:45 -0300

    year35

--- js/humanResourceMachine/year34.js
+++ js/humanResourceMachine/year34.js
@@ -1,4 +1,4 @@
-const year34 = (letras) => {
+/* const year34 = (letras) => {
   const vogais = ['a', 'e', 'i','o', 'u']
 
   if(!vogais.includes(letras)) {
@@ -9,4 +9,16 @@ const year34 = (letras) => {
 year34(5)
 year34('a')
 year34('n')
-year34('u')
+year34('u') */
+
+
+year34(['a', 'b', 2, 3, 4, 5])
+year34(items) {
+   const vogais = ['a', 'e', 'i','o', 'u']
+
+
+    return items.filter((item) => {
+      return !vogais.includes(item)
+
+    })
+} 
---
+++ js/humanResourceMachine/year35.js
@@ -0,0 +1,12 @@
+const year35 = (numero) => {
+  const arr = [1, 2, 2, 4, 5, 5, 5, 6];
+
+  return newArr = [... new Set(arr)]
+
+}
+
+/* console.log(year35([1, 2, 2, 4, 5, 5, 5, 6,])) */
+
+/* console.log(year35(numero)) */
+
+console.log(year35())
