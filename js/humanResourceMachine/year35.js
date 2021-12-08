2c328258029e050d49587fa869a86014de2f97c3
Larissa Gomes <lahgomes@LAPTOP-PUQ31PEG.localdomain>
2021-12-08 07:35:46 -0300

    year36

--- js/humanResourceMachine/year35.js
+++ js/humanResourceMachine/year35.js
@@ -1,12 +1,7 @@
-const year35 = (numero) => {
-  const arr = [1, 2, 2, 4, 5, 5, 5, 6];
-
+const year35 = (arr) => {
+  
   return newArr = [... new Set(arr)]
 
 }
 
-/* console.log(year35([1, 2, 2, 4, 5, 5, 5, 6,])) */
-
-/* console.log(year35(numero)) */
-
-console.log(year35())
+console.log(year35([1, 2, 2, 4, 5, 5, 5, 6]))
---
+++ js/humanResourceMachine/year36.js
@@ -0,0 +1,8 @@
+const year36 = (palavras) => {
+  const arr = palavras.sort()
+
+  return arr[0]
+
+}
+
+console.log(year36(['Banana', 'Tomate', 'Ameixa', 'Pera']))
