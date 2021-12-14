35dfa2f24c86acebbca1978d4941bc900604364e
Larissa Gomes <lahgomes@LAPTOP-PUQ31PEG.localdomain>
2021-12-14 09:19:01 -0300

    year40

---
+++ js/humanResourceMachine/year40.js
@@ -0,0 +1,34 @@
+
+
+const isPrime = (num) => {
+  
+  for(let i = 2; i < num; i++)
+
+    if(num % i === 0) return false;
+
+  return num > 1;
+}
+
+const factor = (num) => {
+
+  const arr = []
+  let counter = num
+
+  while(counter >= 2) {
+
+    if(num % counter === 0) {
+
+      if(isPrime(counter)) {
+
+        arr.push(counter)
+      }     
+    }
+
+    counter--
+  }
+
+  return arr
+}
+
+console.log(factor(12))
+console.log(factor(2))
